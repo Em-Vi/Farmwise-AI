1ef00cdc7dd66fc1c54578ea051e31974abac9dc
Musthu17 <musthumuth1107@gmail.com>
2024-10-31 11:30:13 +0530

    build complete

---
+++ build/precache-manifest.e06528d886b019b10671f94f99587a32.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "370c0cbeeac0764c14cb1d9c21b37a5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc3f21dd180b6dcceeae",
+    "url": "/static/css/main.5afc2a4b.chunk.css"
+  },
+  {
+    "revision": "3a70a6bd2c3488a54321",
+    "url": "/static/js/2.b2450b96.chunk.js"
+  },
+  {
+    "revision": "010bbf9131070847c48b287e24c4bc60",
+    "url": "/static/js/2.b2450b96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc3f21dd180b6dcceeae",
+    "url": "/static/js/main.17767abe.chunk.js"
+  },
+  {
+    "revision": "99f06fdba5239f758573",
+    "url": "/static/js/runtime-main.221f1739.js"
+  },
+  {
+    "revision": "e92b136e483765b89725411cc0300ddc",
+    "url": "/static/media/FarmWise_logo.e92b136e.png"
+  },
+  {
+    "revision": "f8704c65a78e148fb331fb2e9e051697",
+    "url": "/static/media/ai_ml.f8704c65.jpg"
+  },
+  {
+    "revision": "d63d8462cf2d8508ef50c86c50af56cd",
+    "url": "/static/media/green_credits.d63d8462.jpg"
+  },
+  {
+    "revision": "70f4dbef52f8bfe675427fcceb492111",
+    "url": "/static/media/home-garden.70f4dbef.jpg"
+  },
+  {
+    "revision": "b21019516fd574253e2354080dc1dd00",
+    "url": "/static/media/login.b2101951.jpg"
+  },
+  {
+    "revision": "e92b136e483765b89725411cc0300ddc",
+    "url": "/static/media/now-logo.e92b136e.png"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/static/media/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "8894e5bf4b6cafc8184420eddddedac5",
+    "url": "/static/media/realheader.8894e5bf.jpg"
+  }
+]);
